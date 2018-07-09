0b04238eea0a8ae49c95abc7156c380fb06e1b82
Suchitra Naik <susanaik@gmail.com>
2018-07-09 12:50:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const drivers = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendDriver(name){
+  drivers.push(name);
+}
+
+function destructivelyPrependDriver(name){
+  drivers.unshift(name);
+}
+
+
+function destructivelyRemoveLastDriver(name){
+  drivers.pop(name);
+}
+
+function destructivelyRemoveFirstDriver(name){
+  drivers.shift(name);
+}
+
+
+function appendDriver(name){
+  drivers = drivers.slice()
+  const copyOfDrivers = drivers.push(name);
+  return copyOfDrivers;
+}
+
+function prependDriver(name){
+  drivers.splice(0,name);
+}
+
+function removeLastDriver(name){
+  drivers.splice(-1,name);
+}
+
+function removeFirstDriver(name){
+  drivers.splice(0,name);
+}
